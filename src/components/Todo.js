c802722404c451d888bf2ffcec7dece7ddba3a2a
camilla <camillavbueno@gmail.com>
2020-12-17 13:24:41 -0300

    start project

---
+++ src/components/Todo.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Todo = () => {
+
+    return (
+        <div className="todo">
+            <li className="todo-item"></li>
+            <button className="fas fa-check"></button>
+            <button className="fas fa-trah"></button>
+        </div>
+    )
+}
+
+export default Todo;
